29943061f4eae7f6e77648805616aa4264cc369a
root <gigi_berlocea@berlocea.com>
2024-10-29 16:47:12 +0200

    Modified dracu mai stie ce

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -3,11 +3,17 @@ class minecraft {
     ensure => directory,
   }
 
-## KUDOS https://groups.google.com/g/puppet-users/c/ClC2XJXTvnw
-  file { 'minecraft_server.jar':
-    path => '/opt/minecraft/minecraft_server.jar',
-    ensure => file,
-    source => 'puppet:///site/downloads/minecraft_server.jar',
+  file { '/mnt/puppet_nfs':
+    ensure => directory,
+  }
+
+  package { 'nfs-common':
+    ensure => installed,
+  }
+
+  exec { 'mount minecraft and copy it':
+    command  => [ "/bin/bash" , "-c" , "if [ -f /opt/minecraft/minecraft_server.jar ] ; then exit 0 ; fi ; mount puppet:/opt/nfs /mnt/puppet_nfs && cp /mnt/puppet_nfs/minecraft_server.jar /opt/minecraft/ && umount /mnt/puppet_nfs/ " ],
   }
 }
+
 
