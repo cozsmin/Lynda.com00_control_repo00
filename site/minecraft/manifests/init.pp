cb864918633b4c812a2341f95d6e6d743994c677
root <gigi_berlocea@berlocea.com>
2024-10-29 17:13:46 +0200

    Modified/Added site/minecraft/manifests/init.pp, manifests/site.pp and site/minecraft/templates/minecraft.service.erb

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -13,7 +13,27 @@ class minecraft {
 
   exec { 'mount minecraft and copy it':
     command  => [ "/bin/bash" , "-c" , "if [ -f /opt/minecraft/minecraft_server.jar ] ; then exit 0 ; fi ; mount puppet:/opt/nfs /mnt/puppet_nfs && cp /mnt/puppet_nfs/minecraft_server.jar /opt/minecraft/ && umount /mnt/puppet_nfs/ " ],
+    notify => File["/etc/systemd/system/minecraft.service"]
   }
+
+  file { '/etc/systemd/system/minecraft.service':
+    ensure: file,
+	content => template('minecraft/minecraft.service.erb'),
+    # on server at /etc/puppetlabs/code/environments/production/site/minecraft/templates/minecraft.service.erb
+    notify  => Exec["daemon-reload"],
+  }
+
+  exec { 'daemon-reload':
+    command  => [ "/bin/bash" , "-c" , "systemctl daemon-reload" ],
+    notify  => Service["minecraft"],
+  }
+
+  service {'minecraft':
+    name => "minecraft",
+    ensure => running,
+    enable => true,
+  }
+
 }
 
 
