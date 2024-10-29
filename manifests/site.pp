5f207875152b0d4853e1d645c0c7a78d90a9756d
root <gigi_berlocea@berlocea.com>
2024-10-29 15:07:47 +0200

    Modified manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,7 @@ node 'puppet' {
 #    command  => [ "/bin/bash" , "-c" , "if [ -f minecraft_server.jar ] ; then exit 0 ; fi; wget https://piston-data.mojang.com/v1/objects/45810d238246d90e811d896f87b14695b7fb6839/server.jar && mv -f server.jar minecraft_server.jar" ],
     command  => [ "/bin/bash" , "-c" , "wget https://piston-data.mojang.com/v1/objects/45810d238246d90e811d896f87b14695b7fb6839/server.jar && mv -f server.jar minecraft_server.jar" ],
     cwd      => '/etc/puppetlabs/code/environments/production/site/downloads',
-    provider => shell,
+#    provider => shell,
   }
 }
 
@@ -37,4 +37,5 @@ node /^db|^d12c-ps/ {
   include role::db_server
   include role::minecraft
 }
+
 
